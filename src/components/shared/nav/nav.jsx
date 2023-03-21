4392e9c9f3ad7153f04158098ff151e9579460ea
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-21 10:16:51 -0400

    nav bar styling added

--- src/components/shared/nav/nav.jsx
+++ src/components/shared/nav/nav.jsx
@@ -1,5 +1,20 @@
+import Link from 'next/link';
 import styles from './nav.module.scss';
 
 export default function Nav() {
-  return <nav>Sample nav</nav>;
+  const navLinks = ['Home', 'Paintings', 'Exhibitions', 'About', 'Contact'];
+
+  return (
+    <div className='container'>
+      <nav className='navigation'>
+        <ul className='navUl'>
+          {navLinks.map(link => {
+            <li className='navLi'>
+              <Link href={`/${link.toLowerCase()}`} />
+            </li>;
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
 }
