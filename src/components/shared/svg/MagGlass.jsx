b63d8b94fa403f0402183f69d4c0a048f31a1dec
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-22 17:00:06 -0400

    fill color now inherits from the parent element

--- src/components/shared/svg/MagGlass.jsx
+++ src/components/shared/svg/MagGlass.jsx
@@ -7,21 +7,15 @@ export default function MagGlass() {
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <rect width='43.4571' height='39' fill='white' />
+      <rect width='43.4571' height='39' fill='inherit' />
       <path
         d='M18.5313 23.0626C22.1384 23.0626 25.0626 20.1384 25.0626 16.5313C25.0626 12.9242 22.1384 10 18.5313 10C14.9242 10 12 12.9242 12 16.5313C12 20.1384 14.9242 23.0626 18.5313 23.0626Z'
         stroke='#231F20'
-        // stroke-miterlimit='10'
       />
-      <path
-        d='M23.3047 20.5797L25.9601 23.4259'
-        stroke='#231F20'
-        // stroke-miterlimit='10'
-      />
+      <path d='M23.3047 20.5797L25.9601 23.4259' stroke='#231F20' />
       <path
         d='M26.4207 22.8431L24.96 24.2907L29.5392 28.8284L31 27.3809L26.4207 22.8431Z'
         stroke='#231F20'
-        // stroke-miterlimit='10'
       />
     </svg>
   );
