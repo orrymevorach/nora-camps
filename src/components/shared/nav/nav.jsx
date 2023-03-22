d77715c634b9cd2c3bb46edf58c63eab1861531f
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-21 20:02:38 -0400

    join email button added to nav component

--- src/components/shared/nav/nav.jsx
+++ src/components/shared/nav/nav.jsx
@@ -1,20 +1,36 @@
 import Link from 'next/link';
+import EmailListArrow from '../svg/EmailListArrow/EmailListArrow';
+import NoraLogo from '../svg/NoraLogo/NoraLogo';
 import styles from './nav.module.scss';
 
 export default function Nav() {
   const navLinks = ['Home', 'Paintings', 'Exhibitions', 'About', 'Contact'];
 
   return (
-    <div className='container'>
-      <nav className='navigation'>
-        <ul className='navUl'>
-          {navLinks.map(link => {
-            <li className='navLi'>
-              <Link href={`/${link.toLowerCase()}`} />
-            </li>;
-          })}
-        </ul>
-      </nav>
+    <div className={styles.container}>
+      <div className={styles.maxWidth}>
+        <nav className={styles.navigation}>
+          <NoraLogo />
+          <ul className={styles.navUl}>
+            {navLinks.map(link => {
+              return (
+                <li key={link} className={styles.navLi}>
+                  <Link
+                    href={`/${link.toLowerCase()}`}
+                    className={styles.nextLink}
+                  >
+                    {link}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <button className={styles.joinEmailListButton}>
+          <p>Join the Email List</p>
+          <EmailListArrow />
+        </button>
+      </div>
     </div>
   );
 }
