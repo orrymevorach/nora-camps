b7b08802fc6d894c4b7db5cbbc44b8f985283327
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-21 20:01:01 -0400

    EmailListArrow dir created. sub files needed css

---
+++ src/components/shared/svg/EmailListArrow/EmailListArrow.jsx
@@ -0,0 +1,17 @@
+import styles from './EmailListArrow.module.scss';
+
+export default function EmailListArrow() {
+  return (
+    <svg
+      className={styles.arrow}
+      viewBox='0 0 14 14'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M10.5965 6.16664L6.12646 1.69664L7.30479 0.518311L13.7865 6.99998L7.30479 13.4816L6.12646 12.3033L10.5965 7.83331H0.453125V6.16664H10.5965Z'
+        fill='#231F20'
+      />
+    </svg>
+  );
+}
