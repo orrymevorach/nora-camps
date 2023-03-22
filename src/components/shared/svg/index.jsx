4fc66315587c8ef7646d7a89788afdd390d8e684
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-21 22:47:46 -0400

    exports added to index for cleaner imports

--- src/components/shared/svg/index.jsx
+++ src/components/shared/svg/index.jsx
@@ -1,1 +1,4 @@
-export { default } from './svg';
+export { default as RightArrow } from './RightArrow/RightArrow';
+export { default as NoraLogo } from './NoraLogo/NoraLogo';
+export { default as Hamburger } from './Hamburger';
+export { default as MagGlass } from './MagGlass';
