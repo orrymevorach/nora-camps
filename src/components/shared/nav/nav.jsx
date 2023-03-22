28be09e9f56ee398cf6339766bc4e7f396925d71
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-22 12:18:14 -0400

    conditional rendering to change the navbar to show the magnifying glass and hamburger on mobile view

--- src/components/shared/nav/nav.jsx
+++ src/components/shared/nav/nav.jsx
@@ -1,7 +1,10 @@
+import { useWindowSize } from '@/hooks';
 import Link from 'next/link';
 import RightArrow from 'svg/RightArrow/RightArrow';
 import NoraLogo from 'svg/NoraLogo/NoraLogo';
+import MagGlass from 'svg/MagGlass';
 import styles from './nav.module.scss';
+import Hamburger from 'svg/Hamburger';
 
 export default function Nav() {
   const navLinks = [
@@ -12,29 +15,40 @@ export default function Nav() {
     { label: 'Contact', href: '/contact' },
   ];
 
+  const { device } = useWindowSize();
+
   return (
     <div className={styles.container}>
       <div className={styles.maxWidth}>
         <Link href='/'>
           <NoraLogo />
         </Link>
-        <nav className={styles.navigation}>
-          <ul className={styles.navUl}>
-            {navLinks.map(link => {
-              return (
-                <li key={link.label} className={styles.navLi}>
-                  <Link href={link.href} className={styles.nextLink}>
-                    {link.label}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-          <button className={styles.joinEmailListButton}>
-            <p>Join the Email List</p>
-            <RightArrow />
-          </button>
-        </nav>
+        {device === 'mobile' ? (
+          <div className={styles.mobileView}>
+            <MagGlass />
+            <button>
+              <Hamburger />
+            </button>
+          </div>
+        ) : (
+          <nav className={styles.navigation}>
+            <ul className={styles.navUl}>
+              {navLinks.map(link => {
+                return (
+                  <li key={link.label} className={styles.navLi}>
+                    <Link href={link.href} className={styles.nextLink}>
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            <button className={styles.joinEmailListButton}>
+              <p>Join the Email List</p>
+              <RightArrow />
+            </button>
+          </nav>
+        )}
       </div>
     </div>
   );
