77236b62b3391aa94c581db9453a09dbfe3db684
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-22 14:00:38 -0400

    MagGlass component function renamed to match file name

--- src/components/shared/svg/MagGlass.jsx
+++ src/components/shared/svg/MagGlass.jsx
@@ -1,4 +1,4 @@
-export default function MobileSearch() {
+export default function MagGlass() {
   return (
     <svg
       width='44'
@@ -11,17 +11,17 @@ export default function MobileSearch() {
       <path
         d='M18.5313 23.0626C22.1384 23.0626 25.0626 20.1384 25.0626 16.5313C25.0626 12.9242 22.1384 10 18.5313 10C14.9242 10 12 12.9242 12 16.5313C12 20.1384 14.9242 23.0626 18.5313 23.0626Z'
         stroke='#231F20'
-        stroke-miterlimit='10'
+        // stroke-miterlimit='10'
       />
       <path
         d='M23.3047 20.5797L25.9601 23.4259'
         stroke='#231F20'
-        stroke-miterlimit='10'
+        // stroke-miterlimit='10'
       />
       <path
         d='M26.4207 22.8431L24.96 24.2907L29.5392 28.8284L31 27.3809L26.4207 22.8431Z'
         stroke='#231F20'
-        stroke-miterlimit='10'
+        // stroke-miterlimit='10'
       />
     </svg>
   );
