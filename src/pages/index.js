eef414b9a25a0d0ac2aecbf5be933f07e9f1fcea
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-03-23 15:20:07 -0700

    page builder with hero data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,25 @@
+import { getEntryIdsFromPageBuilder } from '@/lib/contentful';
+import { PAGES, getEntryDataFromEntryIds } from '@/utils/contentful';
 import Head from 'next/head';
-// import { Inter } from 'next/font/google';
 
-export default function Home() {
+export default function Home({ entries = [] }) {
+  // Code review notes:
+
+  // the "entries" prop gets populated by getStaticProps, the function at the bottom of this file
+  // Entries will return an array of data that we can use to populate the UI components
+  // An example of the entries data is:
+  // [
+  //   {
+  //     backgroundImage: {
+  //       url: 'https://images.ctfassets.net/t2nx18naire9/5xXJX4QZ…45fe8eff8905c589e6776d88/IMG_Love-Me-Some-3_1.jpg',
+  //       description: 'Background Image',
+  //     },
+  //     contentModel: 'HeroImage',
+  //     description: { json: {} },
+  //     heading: 'Nora Camps',
+  //   },
+  // ];
+
   return (
     <>
       <Head>
@@ -9,4 +27,14 @@ export default function Home() {
       </Head>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const entryIds = await getEntryIdsFromPageBuilder({ page: PAGES.HOME });
+  const entries = await getEntryDataFromEntryIds({ entryIds });
+  return {
+    props: {
+      entries,
+    },
+  };
 }
