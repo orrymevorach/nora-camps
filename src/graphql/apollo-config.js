e103cc9981bfc68301876a9a3c108c657f6cb176
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-03-24 12:23:00 -0700

    change apollog config and add gql package

---
+++ src/graphql/apollo-config.js
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+export const client = new ApolloClient({
+  uri: `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}?access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`,
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: `Bearer ${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`,
+  },
+});
