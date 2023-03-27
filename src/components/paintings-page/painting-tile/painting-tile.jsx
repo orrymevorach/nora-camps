4a98c6d860861a9c19dc0ececd381453dde612e4
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-03-27 13:04:29 -0700

    painting tile component

---
+++ src/components/paintings-page/painting-tile/painting-tile.jsx
@@ -0,0 +1,33 @@
+import { formatPrice } from '@/utils/string-utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './painting-tile.module.scss';
+
+export default function PaintingTile(props) {
+  const { name, imageCollection, price } = props;
+  const firstImage = imageCollection.items[0];
+  return (
+    <Link
+      href={{
+        pathname: '/painting',
+        query: {
+          painting: name,
+        },
+      }}
+      className={styles.paintingTile}
+    >
+      <div className={styles.imageContainer}>
+        <Image
+          src={firstImage.url}
+          alt={firstImage.description}
+          width={firstImage.width}
+          height={firstImage.height}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <p className={styles.paintingName}>{name}</p>
+        <p className={styles.price}>{formatPrice(price)}</p>
+      </div>
+    </Link>
+  );
+}
