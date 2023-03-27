d2d8bdcd5dd3d5135f3b59f0905fdda2f13e775c
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-27 12:26:00 -0400

    nav items now have their own dir and component

---
+++ src/components/shared/nav-bar/nav-items/nav-items.jsx
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { clsx } from 'clsx';
+import CloseButton from '../../svg/close-button';
+import RightArrow from '../../svg/right-arrow/right-arrow';
+import styles from './nav-items.module.scss';
+import { useWindowSize } from '@/hooks';
+
+export default function NavItems({ toggles }) {
+  const { toggleMobileNavView } = toggles;
+  const { isMobile } = useWindowSize();
+  const { route } = useRouter();
+  const navLinks = [
+    { label: 'Home', href: '/' },
+    { label: 'Paintings', href: '/paintings' },
+    { label: 'Exhibitions', href: '/exhibitions' },
+    { label: 'About', href: '/about' },
+    { label: 'Contact', href: '/contact' },
+  ];
+
+  return (
+    <ul className={styles.navUl}>
+      {isMobile && (
+        <li className={styles.closeButton}>
+          <button onClick={() => toggleMobileNavView()}>
+            <CloseButton />
+          </button>
+        </li>
+      )}
+      {navLinks.map(link => {
+        return (
+          <li
+            key={link.label}
+            className={styles.navLi}
+            onClick={() => toggleMobileNavView()}
+          >
+            <Link
+              href={link.href}
+              className={clsx(
+                styles.nextLink,
+                route === link.href ? styles.activeLink : ''
+              )}
+            >
+              {link.label}
+            </Link>
+          </li>
+        );
+      })}
+      {isMobile && (
+        <li className={styles.navLi}>
+          <Link
+            href='/mail'
+            className={clsx(styles.joinEmailList, styles.nextLink)}
+          >
+            <p>Join the Email List</p>
+            <RightArrow />
+          </Link>
+        </li>
+      )}
+    </ul>
+  );
+}
