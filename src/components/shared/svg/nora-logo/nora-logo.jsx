b41f0ee930c368a2ac627a9463dfd0f08fcc1adf
Andrew Boyle <a.michael.boyle@gmail.com>
2023-03-27 21:38:10 -0400

    removed unused class and import of css file

--- src/components/shared/svg/nora-logo/nora-logo.jsx
+++ src/components/shared/svg/nora-logo/nora-logo.jsx
@@ -1,11 +1,10 @@
 import Image from 'next/image';
 import ncLogo from 'images/NC.png';
-import styles from './nora-logo.module.scss';
 
 export default function NoraLogo() {
   return (
     <>
-      <Image className={styles.noraLogo} src={ncLogo} alt='main logo' />
+      <Image src={ncLogo} alt='main logo' />
     </>
   );
 }
