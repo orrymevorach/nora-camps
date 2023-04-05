66204f3ea03b47dfd302e890eb77d1b72c188cd5
Andrew Boyle <a.michael.boyle@gmail.com>
2023-04-05 09:32:03 -0400

    collections drop down ui

--- src/components/paintings-page/collections-drop-down/collections-drop-down.jsx
+++ src/components/paintings-page/collections-drop-down/collections-drop-down.jsx
@@ -1,5 +1,72 @@
-export default function CollectionsDropDown() {
-  // const collections = ['All']
+import { useEffect, useRef, useState } from 'react';
+import DownArrow from '@/components/shared/svg/down-arrow';
+import styles from './collections-drop-down.module.scss';
 
-  return <ul></ul>;
+export default function CollectionsDropDown() {
+  const collections = [
+    'All',
+    'Landscapes',
+    'Abstracts',
+    'Floral',
+    'Portal',
+    'Smalls',
+  ];
+  const [isToggled, setIsToggled] = useState(false);
+  const [dropDownValue, setDropDownValue] = useState('');
+  const inputRef = useRef(null);
+
+  const toggleDropDown = () => {
+    setIsToggled(prev => !prev);
+  };
+
+  useEffect(() => {
+    const updateInputValue = () => {
+      inputRef.current.value = dropDownValue;
+      setIsToggled(false);
+    };
+    if (dropDownValue) {
+      updateInputValue();
+    }
+  }, [dropDownValue]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.borderTop}>
+        <p className={styles.filterBy}>Filter By</p>
+        <div className={styles.dropDown}>
+          <button
+            className={styles.openDropDown}
+            onClick={() => toggleDropDown()}
+          >
+            <input
+              className={styles.input}
+              ref={inputRef}
+              defaultValue='Collections'
+              readOnly
+              tabIndex={-1}
+            />
+            <div className={styles.downArrow}>
+              <DownArrow />
+            </div>
+          </button>
+          {isToggled && (
+            <ul className={styles.ul}>
+              {collections.map(item => {
+                return (
+                  <li
+                    tabIndex={0}
+                    className={styles.li}
+                    onClick={() => setDropDownValue(item)}
+                    key={item}
+                  >
+                    {item}
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
