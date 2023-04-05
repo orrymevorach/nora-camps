c5b06b48a5d44ec8af76b5c3a38f7c154615793f
Andrew Boyle <a.michael.boyle@gmail.com>
2023-04-05 09:38:46 -0400

    newsletter route created

---
+++ src/pages/newsletter.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function Newsletter() {
+  return (
+    <>
+      <Head>
+        <title>Nora Camps | Newsletter</title>
+      </Head>
+    </>
+  );
+}
