3e4e0e283ccacaa803a3fba451896358b7c9baa2
Andrew Boyle <a.michael.boyle@gmail.com>
2023-04-11 20:34:06 -0400

    success svg now receives classNames prop to style the svg

--- src/components/shared/svg/success.jsx
+++ src/components/shared/svg/success.jsx
@@ -1,4 +1,4 @@
-export default function Success() {
+export default function Success({ classNames = '' }) {
   return (
     <svg
       width='14'
@@ -6,6 +6,7 @@ export default function Success() {
       viewBox='0 0 14 14'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
+      className={classNames}
     >
       <circle cx='7' cy='7' r='7' fill='#231F20' />
       <path d='M4 7.91638L6.08398 10L10 5' stroke='white' />
