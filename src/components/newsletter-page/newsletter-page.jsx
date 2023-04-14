f641c4b88b4a3f24da419be0f5d949cfad726c9f
Andrew Boyle <a.michael.boyle@gmail.com>
2023-04-13 22:24:28 -0400

    image added

--- src/components/newsletter-page/newsletter-page.jsx
+++ src/components/newsletter-page/newsletter-page.jsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
 import { useForm } from 'react-hook-form';
 import InputShell from '../shared/form-components/input-shell/input-shell';
 import styles from './newsletter-page.module.scss';
 import EmailInput from '../shared/form-components/email-input/email-input';
 import PrimaryButton from '../shared/primary-button/primary-button';
+import ImageCarousel from '../shared/painting-info-template/image-carousel/image-carousel';
+import ok from 'images/emailListImage.png';
+import Image from 'next/image';
 
 export default function NewsletterPage() {
   const {
@@ -12,13 +14,22 @@ export default function NewsletterPage() {
     handleSubmit,
   } = useForm();
 
+  const image = [
+    {
+      url: '/images/emailListImage.png',
+      width: 404,
+      height: 408.25,
+      alt: 'Abstract painting',
+    },
+  ];
+
   const onSubmit = formData => {
     console.log(formData);
   };
 
   return (
     <div className={styles.container}>
-      {/* <Image /> */}
+      <ImageCarousel images={image} />
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.upperText}>
           <p className={styles.title}>NEWSLETTER</p>
