0f46619ba4c813787449a6b1f356106995bd4fad
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-04-21 18:04:34 -0700

    page spacing

--- src/components/home-page/gallery/gallery.jsx
+++ src/components/home-page/gallery/gallery.jsx
@@ -1,15 +1,18 @@
+import Wrapper from "@/components/shared/wrapper/wrapper";
 import CollectionTile from "../collection-tile";
 
 export default function CollectionsGallery({ items = [] }) {
   return (
-    <ul>
-      {items.map(item => {
-        return (
-          <li key={item.name}>
-            <CollectionTile {...item} />
-          </li>
-        );
-      })}
-    </ul>
+    <Wrapper>
+      <ul>
+        {items.map(item => {
+          return (
+            <li key={item.name}>
+              <CollectionTile {...item} />
+            </li>
+          );
+        })}
+      </ul>
+    </Wrapper>
   );
 }
--- src/components/shared/wrapper/wrapper.jsx
+++ src/components/shared/wrapper/wrapper.jsx
@@ -1,5 +1,9 @@
 import styles from "./wrapper.module.scss";
 
 export default function Wrapper({ children, size = "large" }) {
-  return <div className={styles[`${size}Wrapper`]}>{children}</div>;
+  return (
+    <div className={styles.padding}>
+      <div className={styles[`${size}Wrapper`]}>{children}</div>
+    </div>
+  );
 }
