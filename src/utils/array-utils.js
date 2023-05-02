eab206c1698afa17957064fbdc3901a7928f48b3
Andrew Boyle <a.michael.boyle@gmail.com>
2023-05-01 20:18:52 -0400

    used filter instead of for loop

--- src/utils/array-utils.js
+++ src/utils/array-utils.js
@@ -2,13 +2,8 @@ export const removeCurrentPaintingFromRecommendedList = (
   paintingName,
   allPaintingsResponse
 ) => {
-  const allPaintingsCopy = [...allPaintingsResponse];
-
-  for (let i = 0; i < allPaintingsCopy.length; i++) {
-    if (paintingName === allPaintingsCopy[i].name) {
-      allPaintingsCopy.splice(i, 1);
-      break;
-    }
-  }
-  return allPaintingsCopy.splice(0, 3);
+  const filteredPaintingsArray = allPaintingsResponse.filter(
+    painting => painting.name !== paintingName
+  );
+  return filteredPaintingsArray.splice(0, 3);
 };
