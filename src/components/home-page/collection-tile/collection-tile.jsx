44e00d8745a55887fb9883df832a86566aef2bef
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-05-02 16:24:41 -0700

    add painting tile to collection tile

--- src/components/home-page/collection-tile/collection-tile.jsx
+++ src/components/home-page/collection-tile/collection-tile.jsx
@@ -2,20 +2,24 @@ import Image from "next/image";
 import styles from "./collection-tile.module.scss";
 import PrimaryButton from "@/components/shared/primary-button/primary-button";
 
-export default function CollectionTile({ name, paintingsCollection }) {
-  const firstPaintingInCollection = paintingsCollection.items[0];
-  const image = firstPaintingInCollection.imageCollection.items[0];
+export default function CollectionTile({ name, paintingTile }) {
+  const image = paintingTile?.imageCollection?.items[0];
   return (
     <div className={styles.container}>
-      <Image
-        src={image.url}
-        alt={image.description}
-        width={image.width}
-        height={image.height}
-        className={styles.image}
-        quality={100}
-      />
-      <p className={styles.paintingName}>{firstPaintingInCollection.name}</p>
+      {image && (
+        <Image
+          src={image.url}
+          alt={image.description}
+          width={image.width}
+          height={image.height}
+          className={styles.image}
+          quality={100}
+        />
+      )}
+
+      {paintingTile?.name && (
+        <p className={styles.paintingName}>{paintingTile.name}</p>
+      )}
       <div className={styles.collectionContainer}>
         <div className={styles.left}>
           <p className={styles.collection}>Collection</p>
