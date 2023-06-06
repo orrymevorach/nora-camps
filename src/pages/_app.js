32e2020bb2e42aefd06ee38af3c41c488bf6f4c2
Andrew Boyle <a.michael.boyle@gmail.com>
2023-06-06 14:44:22 -0400

    added google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,14 @@ import Layout from "@/components/shared/layout/layout";
 import "@/styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   const { paintingsAndCollections } = pageProps;
   return (
     <Layout paintingsAndCollections={paintingsAndCollections}>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </Layout>
   );
