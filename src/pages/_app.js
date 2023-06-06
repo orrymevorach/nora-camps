6c27ed4b7ac6dfe48d661d7e7f4b721455e891fa
Andrew Boyle <a.michael.boyle@gmail.com>
2023-06-06 16:11:25 -0400

    hotjar init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import Layout from "@/components/shared/layout/layout";
+import { useEffect } from "react";
+import { hotjar } from "react-hotjar";
 import "@/styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -6,6 +8,14 @@ config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   const { paintingsAndCollections } = pageProps;
+
+  useEffect(() => {
+    hotjar.initialize(
+      process.env.NEXT_PUBLIC_HOTJAR_ID,
+      process.env.NEXT_PUBLIC_HOTJAR_SNIPPET_VERSION
+    );
+  }, []);
+
   return (
     <Layout paintingsAndCollections={paintingsAndCollections}>
       <Component {...pageProps} />
